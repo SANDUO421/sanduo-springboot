f0140a43a018ba028af30e9dab026862a879e5bd
sanduo <sanduo10086@foxmail.com>
2019-04-16 13:20:30 +0800

    完成config-server搭建

--- config-server/src/main/java/com/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/config/server/ConfigServerApplication.java
@@ -1,9 +1,11 @@
-package com.tourism.bigdatatourism;
+package com.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 /**
 * @Description:    配置
 * @Author:         sanduo
@@ -13,10 +15,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
-public class BigdataTourismApplication {
-
+@RefreshScope
+public class ConfigServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BigdataTourismApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
